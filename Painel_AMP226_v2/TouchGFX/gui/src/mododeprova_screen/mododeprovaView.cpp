a90b5ecb7e0966ff16286a43b947b10c93dcd9e1
maiconampera <maicon.douglas@amperaracing.com>
2026-06-04 13:45:39 -0300

    erro na compilação
    ta certo mas a porra deste cube ide esta dando erro na complação quando tenta ler as fontes. QUE SAO GERADAS PELO PROPRIO FILHADAPUTA DO CUBE IDE. to salvando pq vou desinstalar esta merda

--- Painel_AMP226_v2/TouchGFX/gui/src/mododeprova_screen/mododeprovaView.cpp
+++ Painel_AMP226_v2/TouchGFX/gui/src/mododeprova_screen/mododeprovaView.cpp
@@ -1,4 +1,5 @@
 #include <gui/mododeprova_screen/mododeprovaView.hpp>
+#include <touchgfx/Color.hpp>
 
 mododeprovaView::mododeprovaView()
 {
@@ -14,40 +15,107 @@ void mododeprovaView::tearDownScreen()
 {
     mododeprovaViewBase::tearDownScreen();
 }
-void mododeprovaView::updateRPM(int rpm)
+void mododeprovaView::updateRPM(int valor_rpm)
 {
+    if (valor_rpm < 1000) {
+        rotacaouni.setColor(touchgfx::Color::getColorFromRGB(222, 34, 110));
+    }
+    else if (valor_rpm >= 1000 && valor_rpm < 2000) {
+        rotacaouni.setColor(touchgfx::Color::getColorFromRGB(64, 126, 147));
+    }
+    else if (valor_rpm >= 2000 && valor_rpm < 2850) {
+        rotacaouni.setColor(touchgfx::Color::getColorFromRGB(92, 198, 208));
+    }
+    else if (valor_rpm >= 2850 && valor_rpm < 3150) {
+        rotacaouni.setColor(touchgfx::Color::getColorFromRGB(0, 255, 0));
+    }
+    else if (valor_rpm >= 3150 && valor_rpm < 4000) {
+        rotacaouni.setColor(touchgfx::Color::getColorFromRGB(255, 255, 0));
+    }
+    else if (valor_rpm >= 4000 && valor_rpm <= 5000) {
+        rotacaouni.setColor(touchgfx::Color::getColorFromRGB(222, 34, 110));
+    }
+    else {
+        rotacaouni.setColor(touchgfx::Color::getColorFromRGB(255, 0, 0));
+    }
 
+    // Atualiza o texto do RPM
+    Unicode::snprintf(rotacaouniBuffer, ROTACAOUNI_SIZE, "%d", valor_rpm);
+    rotacaouni.invalidate();
+
+    // Atualiza a barra/indicador gráfico do RPM
+    rotacao.setValue(valor_rpm);
+    rotacao.invalidate();
 }
 
-void mododeprovaView::updateTempInversor(int val)
+void mododeprovaView::updateVelocidade(int vel)
 {
+    Unicode::snprintf(velocidadeuniBuffer, VELOCIDADEUNI_SIZE, "%d", vel);
+    velocidadeuni.invalidate();
 
-}
-
-void mododeprovaView::updateTempAcc(int val)
-{
-
+    velocimetro.setValue(vel);
+    velocimetro.invalidate();
 }
 
 void mododeprovaView::updateSOC(int soc)
 {
+	Unicode::snprintf(socuniBuffer, SOCUNI_SIZE, "%d", soc);
+	socuni.invalidate();
 
+    bateria.setValue(soc);
+    bateria.invalidate();
 }
 
-void mododeprovaView::updateFreio(int freio)
+void mododeprovaView::updateFreio(int valor_freio)
 {
+	Unicode::snprintf(freiouniBuffer, FREIOUNI_SIZE, "%d", valor_freio);
+	freiouni.invalidate();
 
+	freiobar.setValue(valor_freio);
+	freiobar.invalidate();
 }
 
 void mododeprovaView::updateAcelerador(int acelera)
 {
+	Unicode::snprintf(aceleradoruniBuffer, ACELERADORUNI_SIZE, "%d", acelera);
+	aceleradoruni.invalidate();
 
+	aceleradorbar.setValue(acelera);
+	aceleradorbar.invalidate();
 }
-void mododeprovaView::updateVelocidade(int vel)
+
+void mododeprovaView::updateTempInversor(int val)
 {
-
+    Unicode::snprintf(tempinvuniBuffer, TEMPINVUNI_SIZE, "%d", val);
+    tempinvuni.invalidate();
 }
+
+void mododeprovaView::updateTempAcc(int temp)
+{
+    Unicode::snprintf(tempaccuniBuffer, TEMPACCUNI_SIZE, "%d", temp);
+
+    if (temp < 20)
+    {
+
+        tempaccuni.setColor(touchgfx::Color::getColorFromRGB(92,198,208));
+    }
+    else if (temp > 40)
+    {
+
+        tempaccuni.setColor(touchgfx::Color::getColorFromRGB(255, 0, 0));
+    }
+    else
+    {
+
+        tempaccuni.setColor(touchgfx::Color::getColorFromRGB(0, 255, 0));
+    }
+
+
+    tempaccuni.invalidate();
+}
+
 void mododeprovaView::updateDistancia(int metros)
 {
-
+    Unicode::snprintf(distanciauniBuffer, DISTANCIAUNI_SIZE, "%d", metros);
+    distanciauni.invalidate();
 }
--- Painel_AMP226_v2/TouchGFX/gui/src/teste_screen/testeView.cpp
+++ Painel_AMP226_v2/TouchGFX/gui/src/teste_screen/testeView.cpp
@@ -1,4 +1,5 @@
 #include <gui/teste_screen/testeView.hpp>
+#include <touchgfx/Color.hpp>
 
 testeView::testeView()
 {
@@ -17,24 +18,75 @@ void testeView::tearDownScreen()
 void testeView::updateFalhaTMS(int falha)
 {
     Unicode::snprintf(falhatmsuniBuffer, FALHATMSUNI_SIZE, "%d", falha);
+
+    if (falha != 0)
+    {
+        tmsok.setVisible(false);
+        falhatmsuni.setVisible(true);
+    }
+    else
+    {
+        tmsok.setVisible(true);
+        falhatmsuni.setVisible(false);
+    }
+
+    tmsok.invalidate();
     falhatmsuni.invalidate();
 }
 
 void testeView::updateFalhaECU(int falha)
 {
     Unicode::snprintf(falhaecuuniBuffer, FALHAECUUNI_SIZE, "%d", falha);
+
+    if (falha != 0)
+    {
+        ecuok.setVisible(false);
+        falhaecuuni.setVisible(true);
+    }
+    else
+    {
+        ecuok.setVisible(true);
+        falhaecuuni.setVisible(false);
+    }
+
+    ecuok.invalidate();
     falhaecuuni.invalidate();
 }
 
 void testeView::updateFalhaINV(int falha)
 {
     Unicode::snprintf(falhainvuniBuffer, FALHAINVUNI_SIZE, "%d", falha);
+
+    if (falha != 0)
+    {
+        invok.setVisible(false);
+        falhainvuni.setVisible(true);
+    }
+    else
+    {
+        invok.setVisible(true);
+        falhainvuni.setVisible(false);
+    }
+
+    invok.invalidate();
     falhainvuni.invalidate();
 }
 
 void testeView::RTDbotao(int rtd)
 {
+    if (rtd != 0)
+    {
+        rtdativado.setVisible(true);
+        rtddesativado.setVisible(false);
+    }
+    else
+    {
+        rtdativado.setVisible(false);
+        rtddesativado.setVisible(true);
+    }
 
+    rtdativado.invalidate();
+    rtddesativado.invalidate();
 }
 
 void testeView::updateSOC(int soc)
@@ -63,8 +115,26 @@ void testeView::updateTempInversor(int temp)
 
 void testeView::updateTempAcc(int temp)
 {
-    Unicode::snprintf(tempaccuniBuffer, TEMPACCUNI_SIZE, "%d", temp);
-    tempaccuni.invalidate();
+	    Unicode::snprintf(tempaccuniBuffer, TEMPACCUNI_SIZE, "%d", temp);
+
+	    if (temp < 20)
+	    {
+
+	        tempaccuni.setColor(touchgfx::Color::getColorFromRGB(92,198,208));
+	    }
+	    else if (temp > 40)
+	    {
+
+	        tempaccuni.setColor(touchgfx::Color::getColorFromRGB(255, 0, 0));
+	    }
+	    else
+	    {
+
+	        tempaccuni.setColor(touchgfx::Color::getColorFromRGB(0, 255, 0));
+	    }
+
+
+	    tempaccuni.invalidate();
 }
 
 void testeView::updateCorrenteHV(float corrente)
@@ -93,12 +163,48 @@ void testeView::updateTensaoInversor(float tesao)
 
 void testeView::updateTensaoCelulaMin(int tesao)
 {
-    Unicode::snprintf(tensaocelminuniBuffer, TENSAOCELMINUNI_SIZE, "%d", tesao);
+    valTensaoMin = (float)tesao / 10.0f;
+
+    Unicode::snprintfFloat(tensaocelminuniBuffer, TENSAOCELMINUNI_SIZE, "%.1f", valTensaoMin);
+
+    float diferenca = valTensaoMax - valTensaoMin;
+    if (diferenca < 0) diferenca = -diferenca;
+
+    if (diferenca >= 0.5f)
+    {
+        tensaocelminuni.setColor(touchgfx::Color::getColorFromRGB(255, 0, 0));
+        tensaocelmaxuni.setColor(touchgfx::Color::getColorFromRGB(255, 0, 0));
+    }
+    else
+    {
+        tensaocelminuni.setColor(touchgfx::Color::getColorFromRGB(255, 255, 255));
+        tensaocelmaxuni.setColor(touchgfx::Color::getColorFromRGB(255, 255, 255));
+    }
+
     tensaocelminuni.invalidate();
+    tensaocelmaxuni.invalidate();
 }
 
 void testeView::updateTensaoCelulaMax(int tesao)
 {
-    Unicode::snprintf(tensaocelmaxuniBuffer, TENSAOCELMAXUNI_SIZE, "%d", tesao);
+    valTensaoMax = (float)tesao / 10.0f;
+
+    Unicode::snprintfFloat(tensaocelmaxuniBuffer, TENSAOCELMAXUNI_SIZE, "%.1f", valTensaoMax);
+
+    float diferenca = valTensaoMax - valTensaoMin;
+    if (diferenca < 0) diferenca = -diferenca;
+
+    if (diferenca >= 0.5f)
+    {
+        tensaocelminuni.setColor(touchgfx::Color::getColorFromRGB(255, 0, 0));
+        tensaocelmaxuni.setColor(touchgfx::Color::getColorFromRGB(255, 0, 0));
+    }
+    else
+    {
+        tensaocelminuni.setColor(touchgfx::Color::getColorFromRGB(255, 255, 255));
+        tensaocelmaxuni.setColor(touchgfx::Color::getColorFromRGB(255, 255, 255));
+    }
+
+    tensaocelminuni.invalidate();
     tensaocelmaxuni.invalidate();
 }
